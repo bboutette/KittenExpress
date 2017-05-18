ad0500445d910dae9253ff64581cbb5a610bb792
b b <bboutette@gmail.com>
2017-05-18 12:29:49 -0500

    testing routes and views.  not yet working.

---
+++ public/javascripts/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+
+// $.getHTML("https://api.github.com/zen", function(data){
+// console.log(data);
+// });
+
+})
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var colors = ["#fd6c3b", "#4edacf","#65a576","#f2d83d"];
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', function(req, res, next) {
+  
+  var color = colors[Math.floor(Math.random()*colors.length)];
+
+  res.render('index', {color: color});
+});
+
+module.exports = router;
